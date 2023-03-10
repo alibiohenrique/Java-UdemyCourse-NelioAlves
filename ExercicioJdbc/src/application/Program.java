64b92315a497c23bfed3459a731c2caae8200263
Henrique Alibio <alibio.henrique@gmail.com>
2023-03-10 18:41:06 -0300

    Basic data update with MySql

--- ExercicioJdbc/src/application/Program.java
+++ ExercicioJdbc/src/application/Program.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -14,39 +10,23 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement ps = null;
 
 		try {
 			conn = DB.getConnection();
-			ps = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES " + "(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			ps = conn.prepareStatement(
+					"UPDATE seller " + "SET BaseSalary = BaseSalary + ? " + "WHERE " + "(DepartmentId = ? )");
+			ps.setDouble(1, 200);
+			ps.setInt(2, 1);
 
-			ps.setString(1, "Josh Yellow");
-			ps.setString(2, "Joshe@yellow.com");
-			ps.setDate(3, new java.sql.Date(sdf.parse("05/05/1989").getTime()));
-			ps.setDouble(4, 35000);
-			ps.setInt(5, 2);
+			int rowsAffected = ps.executeUpdate();
 
-			int rowsAffecteds = ps.executeUpdate();
-			if (rowsAffecteds > 0) {
-				ResultSet rst =  ps.getGeneratedKeys();
-				while (rst.next()) {
-					int id = rst.getInt(1); 
-					System.out.println("Done! Id: " + id);
-				}
-				
-			}
-			else {
-				System.out.println("No rows affected");
-			}
+			System.out.println("Done! Rows updated " + rowsAffected);
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 
-		} catch (ParseException e) {
-			e.printStackTrace();
 		} finally {
 			DB.closeStatement(ps);
 			DB.closeConnection();
