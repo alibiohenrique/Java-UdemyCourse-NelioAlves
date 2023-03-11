57794da840a6e8d1d2458c1dbea3e671601174d7
Henrique Alibio <alibio.henrique@gmail.com>
2023-03-10 21:46:47 -0300

    Created class: Seller

--- ExercicioDemo-Dao-Jdbc/src/application/Program.java
+++ ExercicioDemo-Dao-Jdbc/src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dpt = new Department(1,  "books");
+		Seller seller = new Seller(321, "Jose Carlos", "jose@carlos.com", new Date(), 3500.00, dpt);
 		System.out.println(dpt);
+		System.out.println(seller);
 
 	}
 
