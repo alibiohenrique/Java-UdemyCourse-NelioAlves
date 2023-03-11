a7be2e336a1a68b132ad6e3fa8e89aa5325590e8
Henrique Alibio <alibio.henrique@gmail.com>
2023-03-10 22:29:54 -0300

    Implemented: findById

--- ExercicioDemo-Dao-Jdbc/src/application/Program.java
+++ ExercicioDemo-Dao-Jdbc/src/application/Program.java
@@ -1,23 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department dpt = new Department(1,  "books");
-		Seller seller = new Seller(321, "Jose Carlos", "jose@carlos.com", new Date(), 3500.00, dpt);
-		System.out.println(dpt);
-		System.out.println(seller);
 		
-		SellerDao sellerDao = new DaoFactory().createSellerDao();
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
