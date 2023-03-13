7c234184b1fb8dd5896f289a70fc45bac7858cff
Henrique Alibio <alibio.henrique@gmail.com>
2023-03-13 09:17:23 -0300

    Workshop springboot 3 & jpa

---
+++ Projetowebservices-jpa/src/test/java/com/webservicesjpa/spring/Projetowebservices/jpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicesjpa.spring.Projetowebservices.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
