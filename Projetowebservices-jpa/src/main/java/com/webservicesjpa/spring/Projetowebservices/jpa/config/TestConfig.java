1a7806b87c990956d99baa740df1335b5518f5ed
Henrique Alibio <alibio.henrique@gmail.com>
2023-03-14 09:17:47 -0300

    Created: OrderStatus enum

--- Projetowebservices-jpa/src/main/java/com/webservicesjpa/spring/Projetowebservices/jpa/config/TestConfig.java
+++ Projetowebservices-jpa/src/main/java/com/webservicesjpa/spring/Projetowebservices/jpa/config/TestConfig.java
@@ -10,33 +10,34 @@ import org.springframework.context.annotation.Profile;
 
 import com.webservicesjpa.spring.Projetowebservices.jpa.entities.Order;
 import com.webservicesjpa.spring.Projetowebservices.jpa.entities.User;
+import com.webservicesjpa.spring.Projetowebservices.jpa.entities.enums.OrderStatus;
 import com.webservicesjpa.spring.Projetowebservices.jpa.repositories.OrderRepository;
 import com.webservicesjpa.spring.Projetowebservices.jpa.repositories.UserRepository;
 
-@Configuration 
-@Profile ("test")
-public class TestConfig implements CommandLineRunner{
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
 
-	@Autowired 
+	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");		
-		User u3 = new User(null, "Bob Cyan", "bob@gmail.com", "999999999", "123456");		
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3); 
-		
-		userRepository.saveAll(Arrays.asList(u1,u2,u3));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
-		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Bob Cyan", "bob@gmail.com", "999999999", "123456");
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, u3);
+
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
 	}
-	
+
 }
